eb74a782343ec67fca00e47c55795e19a6c57656
Mario Fernández <nilovinilo1@gmail.com>
2025-07-14 11:47:18 +0200

    Add serviceWorker registration and update index.tsx for PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { register } from "./serviceWorkerRegistration";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
+register();
 
 root.render(
   <React.StrictMode>
